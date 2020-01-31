bee8d3fe8a6c1ab84fde512709cf09eb51f2eddd
wais <waisalmakaleh@gmail.com>
2020-01-31 00:16:22 -0500

    update 6

--- src/components/Navigation/Intro.js
+++ src/components/Navigation/Intro.js
@@ -25,23 +25,31 @@ const Intro = (props) =>
                 <AiOutlineClose className='icon' />
             </Link>
             <div className='introInfo'>
-                <h1>{ "Hello! I'm Wais" }</h1>
-                <h3>{ "I'm a Full Stack Web Developer" }</h3>
-                <h4>My skill as follows</h4>
+                <h1>{ "HI! I'm Wais" }</h1>
+                <h2>{ "I'm a Full Stack Web Developer" }</h2>
+                <h2>My skills as follows</h2>
                 <ul className='list skills'>
                     <li>HTML</li>
                     <li>XML</li>
                     <li>CSS</li>
+                    <li>CSS Animation</li>
                     <li>LESS</li>
                     <li>SASS</li>
+                    <li>GreenSock</li>
+                    <li>Boostrap</li>
                     <li>JavaScript</li>
                     <li>React</li>
                     <li>Redux</li>
-                    <li>Boostrap</li>
                     <li>jQuery</li>
-                    <li>GreenSock</li>
+                    <li>Git</li>
+                    <li>GitHub</li>
+                    <li>Bash</li>
+                    <li>Axios</li>
+                    <li>NPM</li>
+                    <li>Yarn</li>
                     <li>Node.js</li>
                     <li>SQL</li>
+                    <li>{"Restfull Api's"}</li>
                 </ul>
                 <h3>Please feel free to go though my Linkedin to know more about me</h3>
             </div>
