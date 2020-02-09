17cc42f0b51cae701c74d203d91a8eaefa9a09dc
wais <waisalmakaleh@gmail.com>
2020-02-09 11:24:09 -0500

    update info

--- src/components/Navigation/Intro.js
+++ src/components/Navigation/Intro.js
@@ -1,7 +1,9 @@
+import './styles/Intro.scss';
+
 import React, { useEffect, useState } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
-import './styles/Intro.scss'
+
 const Intro = (props) =>
 {
 
@@ -27,31 +29,8 @@ const Intro = (props) =>
             <div className='introInfo'>
                 <h1>{ "HI! I'm Wais" }</h1>
                 <h2>{ "I'm a Full Stack Web Developer" }</h2>
-                <h2>My skills as follows</h2>
-                <ul className='list skills'>
-                    <li>HTML</li>
-                    <li>XML</li>
-                    <li>CSS</li>
-                    <li>CSS Animation</li>
-                    <li>LESS</li>
-                    <li>SASS</li>
-                    <li>GreenSock</li>
-                    <li>Boostrap</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>Redux</li>
-                    <li>jQuery</li>
-                    <li>Git</li>
-                    <li>GitHub</li>
-                    <li>Bash</li>
-                    <li>Axios</li>
-                    <li>NPM</li>
-                    <li>Yarn</li>
-                    <li>Node.js</li>
-                    <li>SQL</li>
-                    <li>{"Restfull Api's"}</li>
-                </ul>
-                <h3>Please feel free to go though my Linkedin to know more about me</h3>
+                
+                <h3>Please feel free to go though my <a href='https://www.linkedin.com/in/wais-almakaleh-43707a158/' target="_blank" className='link'>Linkedin</a> to know more about me</h3>
             </div>
         </div>
     );
--- src/components/Navigation/Work.js
+++ src/components/Navigation/Work.js
@@ -25,7 +25,7 @@ const Work = () =>
                 <AiOutlineClose className='icon' />
             </Link>
             <div className='WorkInfo'>
-                <h1>Hello</h1>
+                <a href='https://drive.google.com/file/d/10diYQxX_-JSnodCS3p5pJv5smvP0utGA/view?usp=sharing' target='_blank#'><h1>Résumé</h1></a>
             </div>
         </div>
     );
