e1ac23594c6f181cd91e87377b5d5556ca9e423e
wais <waisalmakaleh@gmail.com>
2020-02-09 11:28:09 -0500

    forgot to build

--- src/components/Navigation/About.js
+++ src/components/Navigation/About.js
@@ -26,25 +26,7 @@ const About = () =>
                 <AiOutlineClose className='icon' />
             </Link>
             <div className='AboutInfo'>
-                <h1>{ "Hello! I'm Wais" }</h1>
-                <h3>{ "I'm a Full Stack Web Developer" }</h3>
-                <h4>My skill as follows</h4>
-                <ul className='list skills'>
-                    <li>HTML</li>
-                    <li>XML</li>
-                    <li>CSS</li>
-                    <li>LESS</li>
-                    <li>SASS</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>Redux</li>
-                    <li>Boostrap</li>
-                    <li>jQuery</li>
-                    <li>GreenSock</li>
-                    <li>Node.js</li>
-                    <li>SQL</li>
-                </ul>
-                <h3>Please feel free to go though my Linkedin to know more about me</h3>
+                <h1>About</h1>
             </div>
         </div>
     );
